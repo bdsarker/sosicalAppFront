ab01fbfb6ed7b83ebd10ae8ef13212ca3b13bdb0
buddha <buddha@DESKTOP-5M3JG4J>
2025-06-08 18:13:33 -0400

    Fix: Added Routes and member details

--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -1,12 +1,12 @@
 import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AccountService} from '../../core/services/account-service';
-import {Router} from '@angular/router';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 import {ToastService} from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive ],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
